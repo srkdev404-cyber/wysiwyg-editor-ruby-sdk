9ed6bbd477e25ce4e51683bcd10645eb20996515
srkdev404-cyber <srkdev404@gmail.com>
2025-12-16 11:38:58 +0530

    Update version.rb

--- lib/froala-editor-sdk/version.rb
+++ lib/froala-editor-sdk/version.rb
@@ -1,24 +1,8 @@
-- name: Read gem version
-  id: ver
-  run: |
-    FILE="lib/froala-editor-sdk/version.rb"
-
-    if [ ! -f "$FILE" ]; then
-      echo "ERROR: Version file not found: $FILE"
-      exit 1
-    fi
-
-    MAJOR=$(grep -Eo 'Major\s*=\s*[0-9]+' "$FILE" | grep -Eo '[0-9]+')
-    MINOR=$(grep -Eo 'Minor\s*=\s*[0-9]+' "$FILE" | grep -Eo '[0-9]+')
-    TINY=$(grep -Eo 'Tiny\s*=\s*[0-9]+' "$FILE" | grep -Eo '[0-9]+')
-
-    if [ -z "$MAJOR" ] || [ -z "$MINOR" ] || [ -z "$TINY" ]; then
-      echo "ERROR: Could not extract version components"
-      cat "$FILE"
-      exit 1
-    fi
-
-    VERSION="${MAJOR}.${MINOR}.${TINY}"
-
-    echo "VERSION=$VERSION" >> $GITHUB_ENV
-    echo "Extracted version: $VERSION"
+module FroalaEditorSDK 
+  module Version  
+     Major = 4  
+     Minor = 7 
+    Tiny = 1 
+    String = "#{Major}.#{Minor}.#{Tiny}"  
+  end  
+end
